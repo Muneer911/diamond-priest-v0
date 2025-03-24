06ad84d698a2c535c3926ff4422329459badd093
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-03-24 14:30:34 +0300

    implement sign-in form with state management and API integration

--- frontend/src/app/(auth)/signin/page.jsx
+++ frontend/src/app/(auth)/signin/page.jsx
@@ -1,9 +1,34 @@
 // client side
 "use client";
+import { useState } from "react";
 import "./style.css";
 import Link from "next/link";
+import axios from "axios";
 
 export default function signin() {
+  const [formData, SetFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    SetFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:5000/signin",
+        formData
+      );
+      alert(response.data.message);
+    } catch (error) {
+      alert(error.response?.data.error);
+    }
+  };
+
   return (
     <div>
       <main>
@@ -14,31 +39,31 @@ export default function signin() {
               <p>Sign in to access your dashboard</p>
             </div>
 
-            <form action="">
+            <form action="" onSubmit={handleSubmission}>
               <div className="form-group">
                 <label htmlFor="email">Email Address</label>
                 <input
+                  name="email"
                   type="email"
                   className="form-control"
                   id="email"
                   placeholder="Enter your email"
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input
+                  name="password"
                   type="password"
                   className="form-control"
                   id="password"
                   placeholder="Enter your password"
+                  onChange={handleChange}
                 />
               </div>
 
-              <button
-                type="submit"
-                className="btn btn-primary auth-buttons"
-                onClick={() => {}}
-              >
+              <button type="submit" className="btn btn-primary auth-buttons">
                 Sign In
               </button>
             </form>
