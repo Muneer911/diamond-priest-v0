0eb6fcfaf8785f334422d699c716dd0d155f87ad
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-03-29 11:07:33 +0300

    add prediction history tracking and retrieval endpoint in the backend

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, jwt_manager, get_jwt_identity
-from bcrypt import _bcrypt
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 import pandas as pd
 import joblib
 
 preprocessor = joblib.load("../model/preprocessor.joblib")
 model = joblib.load("../model/saved-models/model.joblib")
-
 app = Flask(__name__)
 app.config["JWT-SECRET-KEY"] = "e1c34f338297eddab8cf07cee9699290ca9c1122d951c14c393407a6d6fabbb0f1d7235a368b79a323c098e6a8820832e347dee66782d275e75e833e38609a19d16c641340a81c2791914ed389908aadd2feb1ea7aef4b52cbef43b2dc97dc8f93c79ef2d2598d49834d434eb03eff6316cfc2d07cb99f7ca9328752b7f6ece2c2cf4c6d25dddc4ffb51ebe492706e3ee8fa1a79034f0883bc5fe5de5b61f4c34c57b744af87b1c0ebca3a8364e75a1da31e061b9e4114639b32e33150671eba6be65ed26ffbd6a4e3064d7e6652d73f6ba4b92f29272af1f70cd5bac54a9a8196ae3b24af08ca2c6a109de1e0cc28f48aee2c3621aa21392f5af09fb1ae9b17"
 bcrypt = Bcrypt(app)
-# jwt = jwt_manager(app)
 CORS(app)
-
 users = {"amd.muneer0@gmail.com":"$2a$12$ksYOPHt4sJ1wNPTwO4gO2eZL/enoxAniTQuKso0sahXxvn511vOZO"} # Temporary in-memory database 
 
 
@@ -38,6 +35,7 @@ def signup():
 
     except Exception as e:
         return jsonify(error=f"An error occurred: {str(e)}"), 500
+
 
 @app.route("/signin", methods=["POST", "GET"])
 def signin():
@@ -66,13 +64,14 @@ def signin():
     except Exception as e:
         return jsonify(error=f"An error occurred: {str(e)}"), 500
 
+
+predictionhis = []
 @app.route("/predict", methods=["POST","GET"])
 def predict():
     try:
         # 1. Get the incoming JSON data from the POST request
         incoming_data = request.get_json()
-        print(incoming_data)
-
+        
         if not incoming_data:
             return jsonify({"error": "Invalid JSON input"}), 400
       
@@ -89,16 +88,32 @@ def predict():
         
         # 4. Use the preprocessed data to make a prediction
         prediction = model.predict(processed_data)
+        # Round the predicted price into two decimals 
+        rounded_prediction = round(float(prediction[0]), 2)
+
+        # for entry in incoming_data:
+        #     predictionhis[len()]
+        current_time = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
+        new_entry = {"data" :incoming_data,"price": rounded_prediction, "date": current_time}
+        predictionhis.append(new_entry)
 
         # 5. Return the prediction as a JSON response
-        return jsonify({"prediction": prediction.tolist()})
+        return jsonify(rounded_prediction)
 
     except Exception as e:
         return jsonify(error=f"the error is {str(e)}")
     
 
+@app.route("/gethis", methods=['GET'])
+def gethis():
+    try:
+        # Return the prediction history as a JSON response
+        return jsonify(predictionhis), 200
+    except Exception as e:
+        # Handle any unexpected errors
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+    
 
-# @app.route("/")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
