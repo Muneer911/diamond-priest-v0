19c61eff719327ff6479052553858da8b42d3f52
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-04-15 06:24:30 +0300

    refactor authentication flow and user data handling; integrate Supabase for user signup and signin

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,81 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, jwt_required, jwt_manager, get_jwt_identity
+from flask_jwt_extended import JWTManager
+from supabase import Client, create_client
 from flask_bcrypt import Bcrypt
-from datetime import datetime
+from jose import jwt
+from dotenv import load_dotenv
+
 import pandas as pd
 import joblib
-
+import os
 preprocessor = joblib.load("../model/preprocessor.joblib")
 model = joblib.load("../model/saved-models/model.joblib")
+supabase_url = os.getenv("SUPABASE_URL")
+superbase_apikey = os.getenv("SUPABASE_BEARER_KEY")
+superbase_SK = os.getenv("SUPABASE_JWT_SECRET")
 app = Flask(__name__)
-app.config["JWT-SECRET-KEY"] = "e1c34f338297eddab8cf07cee9699290ca9c1122d951c14c393407a6d6fabbb0f1d7235a368b79a323c098e6a8820832e347dee66782d275e75e833e38609a19d16c641340a81c2791914ed389908aadd2feb1ea7aef4b52cbef43b2dc97dc8f93c79ef2d2598d49834d434eb03eff6316cfc2d07cb99f7ca9328752b7f6ece2c2cf4c6d25dddc4ffb51ebe492706e3ee8fa1a79034f0883bc5fe5de5b61f4c34c57b744af87b1c0ebca3a8364e75a1da31e061b9e4114639b32e33150671eba6be65ed26ffbd6a4e3064d7e6652d73f6ba4b92f29272af1f70cd5bac54a9a8196ae3b24af08ca2c6a109de1e0cc28f48aee2c3621aa21392f5af09fb1ae9b17"
 bcrypt = Bcrypt(app)
 CORS(app)
-users = {"amd.muneer0@gmail.com":"$2a$12$ksYOPHt4sJ1wNPTwO4gO2eZL/enoxAniTQuKso0sahXxvn511vOZO"} # Temporary in-memory database 
+JWT = JWTManager(app)
 
+load_dotenv()
+supabase: Client = create_client(supabase_url,superbase_apikey)
 
-
-@app.route("/", methods=["POST","GET"])
+@app.route("/userdata", methods=["POST","GET"])
 def home():
-    return "Hello, world!"
+    response = request.headers["Authorization"]
+    token = response.split("Bearer ")[1]
+    decoded_token = jwt.decode(token, superbase_SK, algorithms=["HS256"],  audience="authenticated")
+    sub = decoded_token["sub"]
+
+    profile = supabase.from_("id")\
+    .select("*")\
+    .eq("uuid", sub)\
+    .order("user_name")\
+    .order("user_email").execute()
+
+    user_history = supabase.from_("user_history")\
+    .select("*")\
+    .eq("uuid", sub)\
+    .limit(5)\
+    .order("created_at", desc=True)\
+    .execute()
+
+    user_profile = {"user_name": profile.data[0]["user_name"],
+                    "user_email": profile.data[0]["user_email"]}
+    his = user_history.data
+
+
+    return jsonify({"message": "Pass", "user_profile":user_profile, "user_history":his})
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     try:
         # Get the incoming JSON data
         data = request.get_json()
+        username = data["username"]
         email = data["email"]
-        password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+        password = data["password"]
 
         # Store the email and hashed password in the users dictionary
-        users[email] = password
+        response = supabase.auth.sign_up({"email": email, "password": password})
+        if response.user:
+            user_id = response.user.id
+            supabase.table("id").insert({
+                "uuid": user_id,
+                "user_name": username,
+                "user_email": email,
+                "user_hashed_password": password
+            }).execute()
 
-        return jsonify(message="User registered successfully"), 201
+        if "error" in response:
+            return jsonify({"error": response.error.message}), 400
+        
+        return jsonify({"message": "Signup successful"}), 201
 
     except Exception as e:
-        return jsonify(error=f"An error occurred: {str(e)}"), 500
+        return jsonify(error=f"{str(e)}"), 500
 
 
 @app.route("/signin", methods=["POST", "GET"])
@@ -50,19 +91,33 @@ def signin():
         email = signinData["email"]
         password = signinData["password"]
 
-        # Check if the email exists in the users dictionary
-        if email in users:
-            # Validate the password using bcrypt
-            hashed_password = users[email]  # Retrieve the hashed password
-            if bcrypt.check_password_hash(hashed_password, password):
-                return jsonify(message="Logged in successfully"), 200
-            else:
-                return jsonify(error="Invalid password"), 401
-        else:
-            return jsonify(error="Email not found"), 404
+        response = supabase.auth.sign_in_with_password({
+            "email":email,
+            "password":password
+        })
+
+        user = response.user.id
+        if not user:
+            return jsonify({"error":"user not found"})
+        
+        access_token = response.session.access_token
+        profile = supabase.from_("id")\
+            .select("*")\
+            .eq("uuid",user)\
+            .order("user_name")\
+            .execute()
+        
+        return jsonify({"message":"logged in successfully",
+                        "user": {
+                            "id": profile.data[0]["uuid"],
+                            "user_email": profile.data[0]["user_email"],
+                            "user_name": profile.data[0]["user_name"]
+                        },
+                        "access_token": access_token,
+                        "redirect_url": "/Dashboard"})
 
     except Exception as e:
-        return jsonify(error=f"An error occurred: {str(e)}"), 500
+        return jsonify(error=f"{str(e)}"), 500
 
 
 predictionhis = []
@@ -71,8 +126,11 @@ def predict():
     try:
         # 1. Get the incoming JSON data from the POST request
         incoming_data = request.get_json()
+        incoming_header = request.headers["Authorization"]
+        cookie = incoming_header.split("Bearer ")[1]
+        decookie = jwt.decode(cookie, superbase_SK, algorithms=["HS256"], audience="authenticated")
         
-        if not incoming_data:
+        if not incoming_data or not cookie:
             return jsonify({"error": "Invalid JSON input"}), 400
       
         required_columns = ['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']
@@ -91,29 +149,18 @@ def predict():
         # Round the predicted price into two decimals 
         rounded_prediction = round(float(prediction[0]), 2)
 
-        # for entry in incoming_data:
-        #     predictionhis[len()]
-        current_time = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
-        new_entry = {"data" :incoming_data,"price": rounded_prediction, "date": current_time}
-        predictionhis.append(new_entry)
+        # Add "uuid" and "price" to the data dic
+        incoming_data["uuid"] = decookie["sub"]
+        incoming_data["price"] = rounded_prediction
+    
+        supabase.from_("user_history")\
+        .insert([incoming_data]).execute()
 
         # 5. Return the prediction as a JSON response
         return jsonify(rounded_prediction)
 
     except Exception as e:
         return jsonify(error=f"the error is {str(e)}")
-    
-
-@app.route("/gethis", methods=['GET'])
-def gethis():
-    try:
-        # Return the prediction history as a JSON response
-        return jsonify(predictionhis), 200
-    except Exception as e:
-        # Handle any unexpected errors
-        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
-    
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
