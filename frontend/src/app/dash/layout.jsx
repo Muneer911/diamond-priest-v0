4e05b6b3bbab82f2dc1efc14c6c72915f232ccf6
Muneer <ahmed.elsadig.ahmed@gmail.com>
2025-11-06 11:46:36 +0300

    feat(dashboardLayout): apply QueryClientProvider, import QueryClient, and enable "use client"

--- frontend/src/app/dash/layout.jsx
+++ frontend/src/app/dash/layout.jsx
@@ -1,10 +1,15 @@
+"use client";
 import DashBoardHeader from "../components/dashBoardHeader";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 export default function RootLayout({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <>
-      <DashBoardHeader />
-      {children}
+      <QueryClientProvider client={queryClient}>
+        <DashBoardHeader />
+        {children}
+      </QueryClientProvider>
     </>
   );
 }
